22fea59fad030b7ce3fb69702fa06d58fce6fae9
EdMijangos <mloeza2010@hotmail.com>
2018-07-16 12:48:25 -0500

    test

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router  = express.Router();
+const User    = require('')
+const passport = require('passport');
 
-router.get('/signup', (req,res)=>{
-  res.render('/signup');
+//middlewares de autenticacion
+function isAuth(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect('/login')
+  }
+}
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    res.redirect('/')
+  }else{
+    return next();
+  }
+}
+//
+
+router.get('/signup', isLoggedIn, (req,res)=>{
+  res.render('auth/signup');
+})
+
+router.post('/signup', (req,res,next)=>{
+  User.register(req.body, req.body.password) 
+  .then(user=>res.redirect('/login'))
+  .catch(err=>res.send(err))
+})
+
+router.get('/login', isLoggedIn, (req,res)=>{
+  res.render('auth/login')
+})
+
+router.post('/login', passport.authenticate('local'), (req,res,next)=>{
+  res.redirect('/')
 })
 
 
