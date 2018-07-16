58788df85a9ddc1478d9f965c38a369564ab6f18
Rafael González Vázquez <rafaelg@lycklig.com.mx>
2018-07-16 13:19:12 -0500

    merge

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,11 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-<<<<<<< HEAD
 router.post('/', (req,res,next)=>{
   
 });
-=======
 router.get('/profile', (req,res)=>{
   res.render('profile')
 })
->>>>>>> 258cc4f77ebc983f53c60243d4405cde8e599b17
 
 module.exports = router;
