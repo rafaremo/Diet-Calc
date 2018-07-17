aa14f9dc35e8b6f88a49e2b117c2668afdab245d
EdMijangos <mloeza2010@hotmail.com>
2018-07-17 11:02:26 -0500

    seed

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const User    = require('../models/User')
+//const Dieta   = require('../models/Dieta')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/profile', (req,res)=>{
-  res.render('profile')
+router.get('/profile/:id', (req,res)=>{
+  User.findById(req.params.id)
+  .then(user=>{
+  res.render('profile', user)})
+  .catch(err=>res.send(err));
+});
+
+router.get('/results/:id',(req,res)=>{
+  Dieta.findById(req.params.id)
+  .then(user=>{
+  res.render('diet-result', user)})
+  .catch(err=>res.send(err));
 })
 
 module.exports = router;
